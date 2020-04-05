cc1a013508fe0ad89d7d42081cd007d26ac744dd
kirtibhave <kirtibhave1000@gmail.com>
2020-04-05 11:33:42 +0530

    ADDED:Welcome Message

---
+++ src/main/java/com/censusAnalyzer/CensusAnalyzer.java
@@ -0,0 +1,4 @@
+package com.censusAnalyzer;
+
+public class CensusAnalyzer {
+}
---
+++ src/test/java/com/censusAnalyzer/CensusAnalyzerTest.java
@@ -0,0 +1,7 @@
+package com.censusAnalyzer;
+
+public class CensusAnalyzerTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian State Census Information");
+    }
+}
